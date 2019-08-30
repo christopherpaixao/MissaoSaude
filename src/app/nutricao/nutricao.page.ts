c53e712239f7e676858e18f30199f4425e7ac302
unknown <christopherpaixao93@gmail.com>
2019-08-30 08:40:37 -0300

    versão 1.7

--- src/app/nutricao/nutricao.page.ts
+++ src/app/nutricao/nutricao.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
+
 
 @Component({
   selector: 'app-nutricao',
@@ -8,11 +10,21 @@ import { Router } from '@angular/router';
 })
 export class NutricaoPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private iab: InAppBrowser) { }
 
   ngOnInit() {
   }
-  abrirDicas(){
+  abrirDicas() {
     this.router.navigate(['nutricao-dicas']);
+  }
+  abrirUrl(url: string) {
+
+    //setup option
+    const options: InAppBrowserOptions = {
+      zoom: 'no'
+    }
+    //abrir a url
+    const browser = this.iab.create(url, '_self', options);
+
   }
 }
